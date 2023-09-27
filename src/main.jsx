8e6f2df21f8e1ae2cf6ef6e3586c7bdf97c0e924
gamerboywhatcancode <faarisbinrezapc@gmail.com>
2023-09-27 20:35:44 +0600

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import { BrowserRouter, HashRouter, Link } from 'react-router-dom'
+import Home from './components/home.jsx'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+    
+  
+)
