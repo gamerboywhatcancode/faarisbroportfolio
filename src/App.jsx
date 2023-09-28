757383d695f49002eec480e5db721b427dc993a3
gamerboywhatcancode <faarisbinrezapc@gmail.com>
2023-09-29 00:24:13 +0600

    s

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,27 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
-import Home from './assets/pages/home'
-import Skills from './assets/pages/skills.jsx'
-import Interest from './assets/pages/interest'
-import Story from './assets/pages/story'
-import Projects from './assets/pages/projects'
+import Home from './pages/home'
+import Skills from './pages/skills.jsx'
+import Interest from './pages/interest'
+import Story from './pages/story'
+import Projects from './pages/projects'
 function App() {
 
 
 
   return (
     <>
-
+        <BrowserRouter basename='/home'>
+          <Routes>
+        <Route path='/home'element={<Home/>}/>
+        <Route path='/skills' element={<Skills/>}/>
+        <Route path='/interests' element={<Interest/>}/>
+        <Route path='/story' element={<Story/>}/>
+        <Route path='/projects' element={<Projects/>}/>
+      </Routes>
+    <App />
+  </BrowserRouter>
       
       
       
--- src/main.jsx
+++ src/main.jsx
@@ -3,19 +3,12 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, HashRouter, Link } from 'react-router-dom'
-import Home from './assets/pages/home.jsx'
+import Home from './pages/home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/home'>
-      <Routes>
-        <Route path='/home'element={<Home/>}/>
-        <Route path='/skills' element={<Skills/>}/>
-        <Route path='/interests' element={<Interest/>}/>
-        <Route path='/story' element={<Story/>}/>
-        <Route path='/projects' element={<Projects/>}/>
-      </Routes>
-    <App />
-  </BrowserRouter>
+    <React.StrictMode>
+      <App/>
+    </React.StrictMode>
     
   
 )
