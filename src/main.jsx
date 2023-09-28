0e1b576e858a265ed2f25dbf578553b0ef500b6b
gamerboywhatcancode <faarisbinrezapc@gmail.com>
2023-09-28 23:52:30 +0600

    asd

--- src/main.jsx
+++ src/main.jsx
@@ -2,14 +2,32 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, HashRouter, Link } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Link, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/home.jsx'
 
+
+const router = createBrowserRouter([
+  {
+    path: "/faarisbroportfolio/",
+    element: <App/>,
+    children: [
+      {
+        path: '/faarisbroportfolio/',
+        element: <Home/>
+      },
+      {
+        path: "/faarisbroportfolio/skills",
+        element: <Skills/>
+      }
+    ]
+  }
+])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter basename='/'>
-    <Link to='/'/>
+  <React.StrictMode>
     <App />
-  </BrowserRouter>
+  </React.StrictMode>
     
   
 )
