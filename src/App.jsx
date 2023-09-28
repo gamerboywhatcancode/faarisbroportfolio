61e5b3d506b8c0c6bcc3ae25574eb6d8c0e2016d
gamerboywhatcancode <faarisbinrezapc@gmail.com>
2023-09-28 23:47:52 +0600

    yeah so yeah

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,11 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import { Routes,Route } from 'react-router-dom'
-import Home from './components/home'
-import Skills from './components/skills.jsx'
-import Interest from './components/interest'
-import Story from './components/story'
-import Projects from './components/projects'
+import Home from './pages/home'
+import Skills from './pages/skills.jsx'
+import Interest from './pages/interest'
+import Story from './pages/story'
+import Projects from './pages/projects'
 function App() {
 
 
--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter, HashRouter, Link } from 'react-router-dom'
-import Home from './components/home.jsx'
+import Home from './pages/home.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/'>
