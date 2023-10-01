8264e45369cf07a48cd874dddf7b3de8c02d7498
gamerboywhatcancode <faarisbinrezapc@gmail.com>
2023-10-01 15:32:51 +0600

    a

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import './App.css'
 import { Routes,Route, HashRouter as Router } from 'react-router-dom'
 import Home from './pages/home'
 import Skill from './pages/skills'
-import Project from './pages/projects'
+import Project from './pages/Project'
 import Story from './pages/story'
 import Interest from './pages/interest'
 function App() {
