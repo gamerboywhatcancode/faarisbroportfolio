5665cc404f5b89d2c310d439f67a81e0bc74f5aa
gamerboywhatcancode <faarisbinrezapc@gmail.com>
2023-10-01 16:45:22 +0600

    feat: react router

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter, HashRouter, Link, createBrowserRouter } from 'react-router-dom'
+import { BrowserRouter, HashRouter, Link, RouterProvider, createBrowserRouter } from 'react-router-dom'
 import Home from './pages/home.jsx'
 import Skill from './pages/skills.jsx'
 
@@ -28,7 +28,7 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-      <App/>
+      <RouterProvider router={router}/>
   </React.StrictMode>
     
   
